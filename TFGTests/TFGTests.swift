18c454261b177d230df284c3fcf7bd2b9850a229
danielqparra <queraltoies@gmail.com>
2019-03-13 12:29:50 +0100

    Initial Commit

---
+++ TFGTests/TFGTests.swift
@@ -0,0 +1,34 @@
+//
+//  TFGTests.swift
+//  TFGTests
+//
+//  Created by Daniel Queraltó Parra on 13/03/2019.
+//  Copyright © 2019 Daniel Queraltó Parra. All rights reserved.
+//
+
+import XCTest
+@testable import TFG
+
+class TFGTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
